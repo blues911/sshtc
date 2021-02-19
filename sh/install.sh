aa4c1dd630f735d57b7f65ad3c1f99b99e3204ab
smokehill <valera.padolochniy@gmail.com>
2021-02-19 09:51:11 +0300

    sudo usage

--- sh/install.sh
+++ sh/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# sshtc install script
 
 if [ -d /usr/local/sshtc ]; then
     echo "sshtc is already installed"
@@ -8,29 +7,9 @@ fi
 
 fail=0
 
-if [[ "$(whoami)" != "root" ]]; then
-    fail=$((fail + 1))
-    echo "Error: permission denied (run this script as root)"
-fi
-
 if [[ "$(uname)" != "Linux" ]]; then
     fail=$((fail + 1))
     echo "Error: qinfo works only on Linux"
-fi
-
-if [[ "$(which python)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: python not found"
-fi
-
-if [[ "$(which ssh)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: ssh not found"
-fi
-
-if [[ "$(which sshpass)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: sshpass not found"
 fi
 
 if [ ! -d ~/.config ]; then
@@ -43,6 +22,9 @@ if [[ $fail > 0 ]]; then
 fi
 
 echo "Installing..."
-cp -r ./sshtc /usr/local/sshtc
-cp ./sh/sshtc /usr/local/bin/sshtc
+# install system deps
+sudo apt-get install -y python openssh-server sshpass
+# install sshtc
+sudo cp -r ./sshtc /usr/local/sshtc
+sudo cp ./sh/sshtc /usr/local/bin/sshtc
 echo "Ok"
